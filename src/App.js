b5dbef63dc28dd1002a71db4ef68fc1e2bd059ba
quynam <quynamle123@gmail.com>
2022-12-06 13:31:16 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <p>le quy nam</p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
+          Learn React, được edit bởi quý nam
         </a>
       </header>
     </div>
